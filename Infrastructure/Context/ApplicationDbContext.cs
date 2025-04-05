812dee5eeb94da8017202b8ee87e8fa70c7b57be
Oğuzhan <81297977+OguzhanKtn@users.noreply.github.com>
2025-04-05 17:13:58 +0300

    DbContextCreated

---
+++ Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context
+{
+    internal sealed class ApplicationDbContext : IdentityDbContext<AppUser,IdentityRole<Guid>,Guid>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+           builder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        }
+    }
+}
--- Infrastructure/InfrastructureRegistrar.cs
+++ Infrastructure/InfrastructureRegistrar.cs
@@ -1,8 +1,12 @@
-﻿using Application.Interfaces;
+﻿using Domain.Entities;
+using Infrastructure.Context;
 using Infrastructure.Options;
 using Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Scrutor;
 
 namespace Infrastructure;
 
@@ -10,10 +14,36 @@ public static class InfrastructureRegistrar
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<ApplicationDbContext>(opt =>
+        {
+            string connectionString = configuration.GetConnectionString("SqlServer")!;
+            opt.UseSqlServer(connectionString);
+        });
         services.Configure<AzureStorageSettings>(configuration.GetSection("AzureStorage"));
-        services.AddSingleton<IAzureStorageService, AzureStorageService>();
-
         services.Configure<JwtOptions>(configuration.GetSection("Jwt"));
+
+        services
+            .AddIdentity<AppUser, IdentityRole<Guid>>(opt =>
+            {
+                opt.Password.RequiredLength = 6;
+                opt.Password.RequireNonAlphanumeric = true;
+                opt.Password.RequireDigit = true;
+                opt.Password.RequireLowercase = true;
+                opt.Password.RequireUppercase = true;
+                opt.Lockout.MaxFailedAccessAttempts = 5;
+                opt.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                opt.SignIn.RequireConfirmedEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+        services.Scan(opt => opt
+        .FromAssemblies(typeof(InfrastructureRegistrar).Assembly)
+        .AddClasses(publicOnly: false)
+        .UsingRegistrationStrategy(RegistrationStrategy.Skip)
+        .AsImplementedInterfaces()
+        .WithScopedLifetime()
+        );
 
         return services;
     }
